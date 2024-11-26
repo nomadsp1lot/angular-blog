a3271d042588c377da0726ca97e6fde2db9e64ff
Nomads p1lot <nomadspilot@gmail.com>
2024-11-26 08:59:56 -0300

    alteracoes componentes e estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BigCardComponent} from './components/big-card/big-card.component'
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
